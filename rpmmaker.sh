7f7955178222cd012106a391c81d2d4310f77110
friedl <friedl@adiscon.com>
2015-03-17 14:37:38 +0100

    Created RPM's for librdkafka based on Spec file from Magnus Edenhill https://github.com/edenhill/librdkafka/blob/master/rpm/librdkafka.spec Distributed as adiscon-librdkafka

--- rpmmaker.sh
+++ rpmmaker.sh
@@ -1,7 +1,7 @@
 # Definitions common to these scripts
 source $(dirname "$0")/config.sh
 
-#SPECOPTIONS="v5-stable v6-stable v7-stable v7-devel v7-devel-cust v8-stable v8-devel libestr libee libgt librelp json-c liblognorm liblognorm1 liblogging openpgm zeromq3 czmq libmongo-client python-docutils python-sphinx jemalloc gnutls libtasn1 nettle"
+#SPECOPTIONS="v5-stable v6-stable v7-stable v7-devel v7-devel-cust v8-stable v8-devel libestr libee libgt librelp json-c liblognorm liblognorm1 liblogging openpgm zeromq3 czmq libmongo-client python-docutils python-sphinx jemalloc gnutls libtasn1 nettle librdkafka"
 #REPOOPTIONS="v5-stable v7-stable v8-stable v8-devel testing"
 #ARCHOPTIONS="i386 x86_64"				   
 #PLATOPTIONS="epel-5 epel-6 epel-7" #"opensuse-11"
