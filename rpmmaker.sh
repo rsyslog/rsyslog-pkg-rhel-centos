546b9eaa39d23b416b74cffda9f276f465a3b8fe
Andre lorbach <alorbach@adiscon.com>
2019-09-13 10:44:10 +0200

    changed rpmmaker script to support build options from environment
    This is needed for non interactiv build of packages.

--- rpmmaker.sh
+++ rpmmaker.sh
@@ -9,44 +9,66 @@ source $(dirname "$0")/config.sh
 echo "-------------------------------------"
 echo "--- RPMMaker                      ---"
 echo "-------------------------------------"
-echo "SPEC Filebasename?"
-select szSpec in $SPECOPTIONS
-do
-	echo "Making RPM for '$szSpec.spec'
-	"
-	break;
-done
 
-echo "Which Linux Plattform?:"
-select szDist in $PLATOPTIONS "All"
-do
-	case $szDist in "All")
-		szDist=$PLATOPTIONS;
-	esac
-	echo "Making RPM for EPEL '$szDist'
-	"
-        break;
-done
+if [ -z $RPM_SPEC ]; then 
+	echo "SPEC Filebasename?"
+	select szSpec in $SPECOPTIONS
+	do
+		echo "Making RPM for '$szSpec.spec'
+		"
+		break;
+	done
+else 
+        echo "SPEC is set to '$RPM_SPEC'"
+        szSpec=$RPM_SPEC
+fi
 
-echo "Which Architecture?:"
-select szArch in $ARCHOPTIONS "All"
-do
-	case $szArch in "All")
-		szArch=$ARCHOPTIONS;
-	esac
-	echo "Making RPM for Plattforms '$szArch'
-	"
-        break;
-done
+if [ -z $RPM_PLATFORM ]; then
+	echo "Which Linux Plattform?:"
+	select szDist in $PLATOPTIONS "All"
+	do
+		case $szDist in "All")
+			szDist=$PLATOPTIONS;
+		esac
+		echo "Making RPM for EPEL '$szDist'
+		"
+       		break;
+	done
+else
+        echo "PLATFORM is set to '$RPM_PLATFORM'"
+        szDist=$RPM_PLATFORM
+fi
 
-echo "Which YUM repository?":
-select szSubRepo in $REPOOPTIONS "All"
-do
-        case $szSubRepo in "All")
-                szSubRepo=$REPOOPTIONS;
-        esac
-        break;
-done
+if [ -z $RPM_ARCH ]; then
+	echo "Which Architecture?:"
+	select szArch in $ARCHOPTIONS "All"
+	do
+		case $szArch in "All")
+			szArch=$ARCHOPTIONS;
+		esac
+		echo "Making RPM for Plattforms '$szArch'
+		"
+	        break;
+	done
+else
+        echo "ARCH is set to '$RPM_ARCH'"
+        szArch=$RPM_ARCH
+fi
+
+if [ -z $RPM_REPO ]; then
+	echo "Which YUM repository?":
+	select szSubRepo in $REPOOPTIONS "All"
+	do
+	        case $szSubRepo in "All")
+	                szSubRepo=$REPOOPTIONS;
+	        esac
+	        break;
+	done
+else
+        echo "REPO is set to '$RPM_REPO'"
+        szSubRepo=$RPM_REPO
+fi
+
 
 #szRpmBuildDir=/home/makerpm/rpmbuild/SRPMS/
 	
